560fbceef5fcea72e474602e5a7d0b3c315691c9
ngomezcn <naimgomezcn@gmail.com>
2022-05-28 23:14:38 +0200

    Entry point app and simconnect

--- AutoFlightV2/AutoFlightV2.cpp
+++ AutoFlightV2/AutoFlightV2.cpp
@@ -1,8 +1,60 @@
 
-
+#include <windows.h>
+#include <tchar.h>
+#include <stdio.h>
+#include <strsafe.h>
 #include <iostream>
+
+#include "SimConnect.h"
+
+#define _APP_NAME_ "AutoFlightV2"
+
+class SimConnect {
+private:
+	HRESULT hr = NULL;
+	HANDLE* phSimConnect = NULL;
+		
+public:	
+	SimConnect(HANDLE* phSimConnect);
+
+	bool connect();
+};
+
+SimConnect::SimConnect(HANDLE* _phSimConnect) : phSimConnect(_phSimConnect) {
+	
+}
+	
+bool SimConnect::connect() {
+
+	return SUCCEEDED(SimConnect_Open(phSimConnect, _APP_NAME_, NULL, 0, 0, 0));
+}
+
+class Application {
+private:
+	HANDLE hSimConnect = NULL;
+	SimConnect* simConnect = new SimConnect(&hSimConnect);
+	
+public:
+	Application();
+
+	void run();
+};
+
+Application::Application() {
+
+};
+
+void Application::run() {
+
+	if (simConnect->connect()) 
+		std::cout << "Connected to Microsoft Flight Simulator 2020" << std::endl;
+	else
+		return;
+	
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    auto application = Application();
+	application.run();
 }
