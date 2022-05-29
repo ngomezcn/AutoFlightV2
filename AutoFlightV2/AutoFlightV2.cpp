a496aaec26e9977ab6b1b77ef435be4007dea922
ngomezcn <naimgomezcn@gmail.com>
2022-05-29 08:16:27 +0200

    add to definition and request data

--- AutoFlightV2/AutoFlightV2.cpp
+++ AutoFlightV2/AutoFlightV2.cpp
@@ -8,8 +8,23 @@
 #include "SimConnect.h"
 
 
+
+
+
 #ifndef SimConnectManager_h__
 #define SimConnectManager_h__
+
+static enum DATA_DEFINE_ID { // Used in SimConnect_AddToDataDefinition(...)
+	DEFINITION1,
+};
+
+static enum DATA_REQUEST_ID {
+	REQUEST1,
+};
+
+static enum DATA_NAMES { 
+	DATA_VERTICAL_SPEED,
+};
 
 class SimConnectManager {
 private:
@@ -24,6 +39,18 @@ public:
 	bool connect() {
 
 		return SUCCEEDED(SimConnect_Open(phSimConnect, "AutoFlightV2", NULL, 0, 0, 0));
+	}
+
+	void disconnect() {
+		SimConnect_Close(phSimConnect);
+	}
+
+	void addToDataDefinition() {
+		hr = SimConnect_AddToDataDefinition(phSimConnect, DEFINITION1, "Vertical Speed", "Feet per second", SIMCONNECT_DATATYPE_FLOAT32, 0, DATA_VERTICAL_SPEED);
+	}
+
+	void requestData() {
+		hr = SimConnect_RequestDataOnSimObject(phSimConnect, REQUEST1, DEFINITION1,SIMCONNECT_OBJECT_ID_USER, SIMCONNECT_PERIOD_SECOND, SIMCONNECT_DATA_REQUEST_FLAG_CHANGED | SIMCONNECT_DATA_REQUEST_FLAG_TAGGED);
 	}
 };
 #endif // SimConnectManager_h__
@@ -51,7 +78,11 @@ public:
 			return;
 
 		// Add data to definition
-
+		simConnectManager->addToDataDefinition();
+		
+		// Request data
+		simConnectManager->requestData();
+			
 		// ....
 
 		// Loop
