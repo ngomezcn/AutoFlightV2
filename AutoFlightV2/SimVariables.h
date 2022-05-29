48ea0a3b141c1b03a6b3ede1b5201b6bdf64000a
ngomezcn <naimgomezcn@gmail.com>
2022-05-29 21:55:43 +0200

    Adding long term todo

--- AutoFlightV2/SimVariables.h
+++ AutoFlightV2/SimVariables.h
@@ -6,6 +6,8 @@
 #include <Windows.h>
 #include <SimConnect.h>
 
+// TODO: Categorize by engine, flight model, electrics, control, autopilot, landing/brake, fuel and misc | See: https://docs.flightsimulator.com/html/Programming_Tools/SimVars/Simulation_Variables.htm
+
 struct SIM_VARIABLES {
 	double THROTTLE1;
 	double THROTTLE2;
